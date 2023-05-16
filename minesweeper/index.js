7cbaf95934e87704f1da056b17f136b93c5f6753
ulja4a <ulja4a@gmail.com>
2023-05-16 16:25:30 +0400

    feat: add coordinates for cells

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -3,51 +3,33 @@
 let size = 10;
 let bombFrequency = 0.1;  //Коэффициент бомб
 let bombNumber = size * bombFrequency;
-let cellSize = 40;  //Размер клетки
-let cells = size * size;
-let bombs = [];
-let numbers = [];
-let numberColors = ['#3498db', '#2ecc71', '#e74c3c', '#9b59b6', '#f1c40f', '#1abc9c', '#34495e', '#7f8c8d',];
+let cells;
+//let bombs = [];
+//let numbers = [];
+//let numberColors = ['#3498db', '#2ecc71', '#e74c3c', '#9b59b6', '#f1c40f', '#1abc9c', '#34495e', '#7f8c8d',];
 const title = document.createElement('h1');
+const wrap = document.createElement('div');
 
 document.body.classList.add('body');
 title.classList.add('title');
 title.textContent = 'Minesweeper';
 document.body.appendChild(title);
+wrap.classList.add('grid-wrap');
+document.body.appendChild(wrap);
 
 
-function startGame(size, bombNumber) {
+const startGame = () => {
   const grid = document.createElement('div');
   grid.classList.add('grid');
-  document.body.appendChild(grid);
+  wrap.appendChild(grid);
 
   for (let i = 0; i < size; i++) {
-      for (let j = 0; j < size; j++) {
+    for (let j = 0; j < size; j++) {
           const cell = document.createElement('div');
           cell.classList.add('cell');
+          cell.setAttribute('data-tile', `${i},${j}`); //Добавляем к клеткам координаты
           grid.appendChild(cell);
-      }
+    }
   }
 }
-startGame(size, bombNumber);
-
-// Функция для создания поля
-/*function createBoard() {
-  const grid = [];
-  for (let w = 0; w < size; w++) {
-    const row = [];
-    for (let h = 0; h < size; h++) {
-      const cell = {w, h}
-      row.push(cell);
-    }
-    grid.push(row);
-  }
-  return grid;
-}*/
-
-/*const size = 10;
-const bombNumber = 10;
-console.log(createBoard(10, 10));
-grid = createBoard(size, bombNumber);
-let bombs = [];
-let numbers = [];*/
+startGame();
