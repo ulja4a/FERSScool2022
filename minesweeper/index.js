ce26e81cfbda59296583eb8e62f06f342390e61c
ulja4a <ulja4a@gmail.com>
2023-05-17 23:14:26 +0400

    fix: add bombs and

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -5,6 +5,7 @@ document.body.classList.add('body');
 title.classList.add('title');
 title.textContent = 'Minesweeper';
 document.body.appendChild(title);
+
 wrap.classList.add('grid-wrap');
 document.body.appendChild(wrap);
 
@@ -20,14 +21,27 @@ let cells = [];
 
 //Создание поля
 function createGrid() {
-  
+  const bombs = Array(bombAmount).fill('bomb'); // Создаем массив из бомб со значением bomb
+  console.log(bombs);
+  const emptySquares = Array((size*size) - bombAmount).fill('valid'); //Создаем массив оставшихся валидніх пустіх клеток
+  console.log(emptySquares);
+  const board = emptySquares.concat(bombs); //Объединяем массивы в один
+  console.log(board);
+  const randomBoard = board.sort(() => Math.random()-0.5); // Рандомно распределяем бомбы
+  console.log(randomBoard);
 
   for (let i = 0; i < size * size; i++) {
-          const cell = document.createElement('div');
-          cell.classList.add('cell');
-          grid.appendChild(cell);
-          cells.push(cell);
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    if (randomBoard[i] === 'bomb') {
+      cell.classList.add('bomb');
     }
+    if (randomBoard[i] === 'valid') {
+      cell.classList.add('valid');
+    }
+    grid.appendChild(cell);
+    cells.push(cell);
+}
 }
 
 createGrid();
