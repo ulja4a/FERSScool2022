98b96a0b2d01bb464dd6f29fc9b90124d811b45f
ulja4a <ulja4a@gmail.com>
2023-05-17 21:55:54 +0400

    fix: chenge code grid and css for it

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -1,12 +1,3 @@
-
-
-let size = 10;
-let bombFrequency = 0.1;  //Коэффициент бомб
-let bombNumber = size * bombFrequency;
-let cells;
-//let bombs = [];
-//let numbers = [];
-//let numberColors = ['#3498db', '#2ecc71', '#e74c3c', '#9b59b6', '#f1c40f', '#1abc9c', '#34495e', '#7f8c8d',];
 const title = document.createElement('h1');
 const wrap = document.createElement('div');
 
@@ -18,18 +9,25 @@ wrap.classList.add('grid-wrap');
 document.body.appendChild(wrap);
 
 
-const startGame = () => {
-  const grid = document.createElement('div');
+const grid = document.createElement('div');
   grid.classList.add('grid');
   wrap.appendChild(grid);
 
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
+let size = 10;
+let bombAmount = 10;
+let cells = [];
+
+
+//Создание поля
+function createGrid() {
+  
+
+  for (let i = 0; i < size * size; i++) {
           const cell = document.createElement('div');
           cell.classList.add('cell');
-          cell.setAttribute('data-tile', `${i},${j}`); //Добавляем к клеткам координаты
           grid.appendChild(cell);
+          cells.push(cell);
     }
-  }
 }
-startGame();
+
+createGrid();
